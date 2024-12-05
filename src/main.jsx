7e904b77ecbd4069c432033d49e1a071ee0cf890
uzwal01 <uzwalduwal07@gmail.com>
2024-12-05 09:47:13 +0545

    3rd change

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { HashRouter } from 'react-router-dom'
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+    <HashRouter>
+      <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
+    </HashRouter>
+    
+  
+  
+  
+  
 )
